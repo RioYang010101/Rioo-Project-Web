a771008aee3915a6472cf28c62953aa9efb113f6
Rioo <ruoilin4@gmail.com>
2025-08-08 12:14:50 +0800

    Update growagarden.js

--- api/growagarden.js
+++ api/growagarden.js
@@ -9,9 +9,21 @@ module.exports = {
     try {
       const response = await fetch("https://suraweb.my.id/info/stockgarden");
       const data = await response.json();
-      res.json({ status: true, data });
+
+      // Ubah Creator jika ada
+      if (data.Creator) {
+        data.Creator = "Rioo";
+      }
+
+      res.json({
+        status: true,
+        data
+      });
     } catch (err) {
-      res.status(500).json({ status: false, error: err.message });
+      res.status(500).json({
+        status: false,
+        error: err.message
+      });
     }
   }
 };
